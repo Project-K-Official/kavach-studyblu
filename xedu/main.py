72fe0b5aab25ab2eaada61f673253dfbd0501430
Jatin Jindal <jatinjindal.2003@gmail.com>
2022-09-02 14:49:09 +0530

    Refactor

--- xedu/main.py
+++ xedu/main.py
@@ -32,19 +32,24 @@ class LandingPage(QMainWindow):
 
         self.setWindowTitle("StudyBlu")
         # Finding the button
-        # button1  -> Get Started
         # button7  -> Exit
         # button8  -> Share
         # button9  -> Course_1
         # button10 -> Course_2
         # button11 -> Course_3
 
+<<<<<<< Updated upstream
         # self.button1 = self.findChild(QPushButton, "pushButton")
         # self.button1.clicked.connect(self.gotocp)
+=======
+        self.shareButton = self.findChild(QPushButton, "pushButton_8")
+        self.shareButton.clicked.connect(self.share)
+>>>>>>> Stashed changes
 
-        self.button8 = self.findChild(QPushButton, "pushButton_8")
-        self.button8.clicked.connect(self.share)
+        self.backButton = self.findChild(QPushButton, "pushButton_7")
+        self.backButton.clicked.connect(self.exit)
 
+<<<<<<< Updated upstream
         # self.button7 = self.findChild(QPushButton, "pushButton_7")
         # self.button7.clicked.connect(self.exit)
 
@@ -56,15 +61,15 @@ class LandingPage(QMainWindow):
 
         self.button11 = self.findChild(QPushButton, "course_no_3")
         self.button11.clicked.connect(lambda: self.goto_rp(3))
+=======
+        for i in range(1, 4):
+            self.findChild(QPushButton, f"course_no_{i}")\
+                .clicked.connect(lambda: self.goto_rp(i))
+>>>>>>> Stashed changes
 
     def share(self):
         url = QUrl("https://www.kavach.org.in/")
         QDesktopServices.openUrl(url)
-
-    def gotocp(self):
-        screen2 = CourseInfo()
-        widget.addWidget(screen2)
-        widget.setCurrentIndex(widget.currentIndex()+1)
 
     def exit(self):
         sys.exit(app.exec())
@@ -76,68 +81,6 @@ class LandingPage(QMainWindow):
 
 # *********************************************
 #                  FRAME 2
-# *********************************************
-
-
-class CourseInfo(QMainWindow):
-    def __init__(self):
-        super(CourseInfo, self).__init__()
-        uic.loadUi("./ui/course_info.ui", self)
-
-        # On-clicking this button python will be executing the function gotorp and showing the app
-        # button -> Start Reading
-        # button1 -> terminal
-        # button2 -> Quiz
-        # button3 -> Back
-        # button4 -> terminal game
-
-        self.button = self.findChild(QPushButton, "pushButton")
-        self.button.clicked.connect(self.gotorp)
-
-        self.button2 = self.findChild(QPushButton, "pushButton_5")
-        self.button2.clicked.connect(self.gotoq)
-
-        self.button3 = self.findChild(QPushButton, "pushButton_3")
-        self.button3.clicked.connect(self.back)
-
-        self.button4 = self.findChild(QPushButton, "pushButton_4")
-        self.button4.clicked.connect(self.terminalgame)
-
-        self.button5 = self.findChild(QPushButton, "pushButton_2")
-        self.button5.clicked.connect(self.share)
-        # not sure what the error is but this left uncommented gives:
-        # "AttributeError: 'NoneType' object has no attribute 'clicked'" when i click the start reading button on the landing page
-        # self.button5 = self.findChild(QPushButton, "pushButton_2")
-        # self.button5.clicked.connect(self.share)
-
-    def gotorp(self):
-        screen2 = ReadingPage(1)
-        widget.addWidget(screen2)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-        # print(widget.currentIndex()+1)
-
-    def back(self):
-        screen1 = LandingPage()
-        widget.addWidget(screen1)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-        # print(widget.currentIndex()+1)
-
-    def gotoq(self):
-        screen1 = QuizGame()
-        widget.addWidget(screen1)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-
-    def terminalgame(self):
-        screen1 = TerminalGame()
-        widget.addWidget(screen1)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-
-    def share(self):
-        url = QUrl("https://www.google.co.in/")
-        QDesktopServices.openUrl(url)
-
-# *********************************************
-#                  FRAME 3
 # *********************************************
 
 
@@ -165,221 +108,6 @@ class ReadingPage(QMainWindow):
         screen1 = LandingPage()
         widget.addWidget(screen1)
         widget.setCurrentIndex(widget.currentIndex()+1)
-
-
-# *********************************************
-#                  Terminal Game
-# *********************************************
-
-class TerminalGame(QMainWindow):
-    def __init__(self):
-        super(TerminalGame, self).__init__()
-        uic.loadUi("./ui/terminal_game.ui", self)
-
-        self.button = self.findChild(QPushButton, "pushButton")
-        self.button.clicked.connect(self.back)
-
-    def back(self):
-        screen1 = CourseInfo()
-        widget.addWidget(screen1)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-        print(widget.currentIndex()+1)
-
-# *********************************************
-#                  Score Page
-# *********************************************
-
-
-class QuizScore(QMainWindow):
-    def __init__(self, score):
-        super(QuizScore, self).__init__()
-        uic.loadUi("./ui/quiz_score.ui", self)
-        self.QuestionText.setText(str(score))
-
-        self.button = self.findChild(QPushButton, "pushButton")
-        self.button.clicked.connect(self.back)
-
-    def back(self):
-        screen1 = CourseInfo()
-        widget.addWidget(screen1)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-        print(widget.currentIndex()+1)
-
-# *********************************************
-#                  Quiz Game
-# *********************************************
-
-
-class QuizGame(QMainWindow):
-    def __init__(self):
-        super(QuizGame, self).__init__()
-        uic.loadUi("/ui/quiz_game.ui", self)
-
-        self.button = self.findChild(QPushButton, "pushButton")
-        self.button.clicked.connect(self.back)
-
-        self.parameters = {
-            "question": [],
-            "answer1": [],
-            "answer2": [],
-            "answer3": [],
-            "answer4": [],
-            "correct": [],
-            "score": [],
-            "index": [],
-            "hint": [],
-            "pAnsExp": [],
-            "questionNo": []
-        }
-
-        self.clear_parameters()
-        self.preload_data(self.parameters["index"][-1])
-        self.quiz()
-
-    f = open('quiz/data.json')
-    data = json.load(f)
-    df = pd.DataFrame(data["results"])
-    # load 1 instance of questions & answers at a time from the database
-
-    def back(self):
-        screen1 = CourseInfo()
-        widget.addWidget(screen1)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-        # print(widget.currentIndex()+1)
-
-    def quiz(self):
-        # Score
-        score = self.parameters["score"][-1]
-        self.ScoreCounter.setText(str(score))
-
-        # Question
-        question = self.parameters["question"][-1]
-        questionNo = self.parameters["questionNo"][-1]
-        self.QuestionText.setText(str(questionNo) + ". " + str(question))
-
-        # Answers
-        answer1 = self.parameters["answer1"][-1]
-        self.Option1.setText(answer1)
-        self.Option1.clicked.connect(lambda x: self.is_correct(self.Option1))
-
-        answer2 = self.parameters["answer2"][-1]
-        self.Option2.setText(answer2)
-        self.Option2.clicked.connect(lambda x: self.is_correct(self.Option2))
-
-        answer3 = self.parameters["answer3"][-1]
-        self.Option3.setText(answer3)
-        self.Option3.clicked.connect(lambda x: self.is_correct(self.Option3))
-
-        answer4 = self.parameters["answer4"][-1]
-        self.Option4.setText(answer4)
-        self.Option4.clicked.connect(lambda x: self.is_correct(self.Option4))
-
-        # Hint
-        hint = self.parameters["hint"][-1]
-        self.SHORTCheck.setText("")
-        self.AnswerExplanation.setText(hint)
-
-        self.NextQuestion.setEnabled(False)
-        self.button = self.findChild(QPushButton, "NextQuestion")
-        self.button.clicked.connect(self.next)
-
-    def preload_data(self, idx):
-        # idx parm: selected randomly time and again at function call
-        question = self.df["question"][idx]
-        correct = self.df["correct_answer"][idx]
-        wrong = self.df["incorrect_answers"][idx]
-        hint = self.df["hint"][idx]
-        answerexp = self.df["AnswerExp"][idx]
-
-        # store local values globally
-        self.parameters["question"].append(question)
-        self.parameters["correct"].append(correct)
-
-        all_answers = wrong + [correct]
-        random.shuffle(all_answers)
-
-        self.parameters["answer1"].append(all_answers[0])
-        self.parameters["answer2"].append(all_answers[1])
-        self.parameters["answer3"].append(all_answers[2])
-        self.parameters["answer4"].append(all_answers[3])
-        self.parameters["hint"].append(hint)
-        self.parameters["pAnsExp"].append(answerexp)
-
-        # print correct answer to the terminal (for testing)
-        print(self.parameters["question"][-1])
-        print(self.parameters["correct"][-1])
-
-    def clear_parameters(self):
-        # clear the global dictionary of parameters
-        for parm in self.parameters:
-            if self.parameters[parm] != []:
-                for i in range(0, len(self.parameters[parm])):
-                    self.parameters[parm].pop()
-        # populate with initial index & score values
-        random_index = [x for x in range(7)]
-        random.shuffle(random_index)
-        self.parameters["index"] = random_index + self.parameters["index"]
-        self.parameters["score"].append(0)
-        self.parameters["questionNo"].append(1)
-
-    def is_correct(self, btn):
-        # a function to evaluate wether user answer is correct
-        self.Option1.setEnabled(False)
-        self.Option2.setEnabled(False)
-        self.Option3.setEnabled(False)
-        self.Option4.setEnabled(False)
-        self.NextQuestion.setEnabled(True)
-
-        if (btn.text() == self.parameters["correct"][-1]):
-            # CORRECT ANSWER
-
-            print(self.parameters["index"])
-            print(len(self.parameters["index"]))
-
-            if (len(self.parameters["index"]) != 0):
-                # update score (+10 points)
-                temp_score = self.parameters["score"][-1]
-                self.parameters["score"].pop()
-                self.parameters["score"].append(temp_score + 10)
-                self.ScoreCounter.setText(str(self.parameters["score"][-1]))
-                self.SHORTCheck.setText("Correct Answer!")
-        else:
-            self.SHORTCheck.setText("WRONG ANSWER!")
-            ansexp = self.parameters["pAnsExp"][-1]
-            self.AnswerExplanation.setText(ansexp)
-
-    def next(self):
-        self.Option1.setEnabled(True)
-        self.Option2.setEnabled(True)
-        self.Option3.setEnabled(True)
-        self.Option4.setEnabled(True)
-        self.NextQuestion.setEnabled(False)
-
-        # if (len(self.parameters["index"]) > 1):
-        if (self.parameters["questionNo"][-1] < 7):
-            temp_no = self.parameters["questionNo"][-1]
-            self.parameters["questionNo"].pop()
-            self.parameters["questionNo"].append(temp_no + 1)
-            print("Question No: ", self.parameters["questionNo"][-1])
-
-            self.parameters["index"].pop()
-            self.preload_data(self.parameters["index"][-1])
-            self.ScoreCounter.setText(str(self.parameters["score"][-1]))
-            self.QuestionText.setText(
-                str(self.parameters["questionNo"][-1]) + ". " + self.parameters["question"][-1])
-            self.Option1.setText(self.parameters["answer1"][-1])
-            self.Option2.setText(self.parameters["answer2"][-1])
-            self.Option3.setText(self.parameters["answer3"][-1])
-            self.Option4.setText(self.parameters["answer4"][-1])
-
-            hint = self.parameters["hint"][-1]
-            self.SHORTCheck.setText("")
-            self.AnswerExplanation.setText(hint)
-
-        else:
-            screen1 = QuizScore(self.parameters["score"][-1])
-            widget.addWidget(screen1)
-            widget.setCurrentIndex(widget.currentIndex()+1)
 
 
 if __name__ == '__main__':
